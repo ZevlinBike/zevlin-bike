4db35e425089aeb8c4584b88224d752f149bb7ef
Tyler Lundin <82530947+Tyler-Lundin@users.noreply.github.com>
2025-08-09 11:04:07 -0700

    Update page.tsx

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@/components/auth/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <div className="w-full max-w-md">
+    <div className="w-full max-w-md pt-40">
       <RegisterForm />
     </div>
   );
