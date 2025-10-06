4f636ed6256dbef806697d60db2f6c410ab7ac5b
Tyler Lundin <TylerJamesLundin@gmail.com>
2025-10-05 21:17:17 -0700

    Installed vercel speed insights for dev utilization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import { createClient } from "@/lib/supabase/server";
 import Navigation from "@/components/Navigation";
@@ -46,6 +47,7 @@ export default async function RootLayout({
       >
         <SocialFABs />
         <Analytics />
+        <SpeedInsights />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
